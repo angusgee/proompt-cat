6e8bca242751157ae6e968d7c84d82586b32972e
MaltaWebDev <MaltaWebDev@gmail.com>
2024-07-29 22:31:12 +0200

    Refactor code
    - Create new func to read all files from all dirs and subdirs recursively
    - Add new async main function
    
    Script now returns all filepaths correctly.

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import gradient from "gradient-string";
 import util from "util";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 
 const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
 const figletPromise = util.promisify(figlet);
-const readdir = util.promisify(fs.readdir);
-const readFile = util.promisify(fs.readFile);
-const stat = util.promisify(fs.stat);
 
 const fileExtentionsToExclude = [
   ".git",
@@ -63,81 +60,104 @@ const fileExtentionsToExclude = [
 
 async function startingTokenCount() {
   console.clear();
-
   const msg = `Proompt Cat`;
-
   const data = await figletPromise(msg);
-
   console.log(gradient.pastel.multiline(data));
-
   const animatedTitle = chalkAnimation.rainbow(
     "Analysing project...\nCounting tokens..."
   );
-
   await sleep();
   animatedTitle.stop();
 }
 
-async function readFiles() {
-  const currentDirectory = process.cwd();
-  const finalStringArray = [];
+async function readAllFiles(dir) {
+  const fileList = [];
+  try {
+    const items = await fs.readdir(dir, { withFileTypes: true });
+    for (const item of items) {
+      const fullPath = path.join(dir, item.name);
 
-  // read files
-  const files = await readdir(currentDirectory);
-  console.log("list of files to process: ", files);
-
-  // create new array with unwanted file types removed
-  const cleanedArray = [];
-
-  for (const file of files) {
-    const fileParts = file.split(".");
-    const fileExtension = "." + fileParts[fileParts.length - 1];
-    // console.log(fileExtension);
-    if (!files.includes(fileExtension)) {
-      cleanedArray.push(file);
+      if (item.isDirectory()) {
+        fileList.push(...(await readAllFiles(fullPath)));
+      } else if (item.isFile()) {
+        fileList.push(fullPath);
+      }
     }
+  } catch (error) {
+    console.log("error reading directory: ", dir, error);
   }
-
-  console.log(
-    "cleaned array with unwanted file extentions removed: ",
-    cleanedArray
-  );
-
-  // iterate over files
-  for (const file of cleanedArray) {
-    // exclude package-lock file
-    if (file === "package-lock.json") {
-      continue;
-    }
-
-    const filePath = path.join(currentDirectory, file);
-
-    // exclude node modules folder
-    if (filePath.includes("node_modules/")) {
-      continue;
-    }
-
-    const fileStat = await stat(filePath);
-    if (fileStat.isDirectory()) {
-      continue;
-    }
-
-    // read contents and append to final string array
-    // TO-DO - add filenames and delimiters
-    const content = await readFile(filePath, "utf-8");
-    finalStringArray.push(content);
-  }
-
-  return finalStringArray;
+  return fileList;
 }
 
-(async function () {
+async function main() {
   await startingTokenCount();
-  const finalStringArray = await readFiles();
-  const finalString = finalStringArray.join("\n");
-  // console.log("final string array: ", finalStringArray);
-  console.log("final string: ", finalString);
-})();
+  const dirPath = process.cwd();
+  const files = await readAllFiles(dirPath);
+  console.log(files);
+}
+
+main().catch(console.error);
+
+// async function readFiles() {
+//   const currentDirectory = process.cwd();
+//   const finalStringArray = [];
+
+//   // read files
+//   const files = await readdir(currentDirectory);
+//   console.log("list of files to process: ", files);
+
+//   // create new array with unwanted file types removed
+//   const cleanedArray = [];
+
+//   // check file extension is not blacklisted, add to new array
+//   for (const file of files) {
+//     const fileParts = file.split(".");
+//     const fileExtension = "." + fileParts[fileParts.length - 1];
+//     // console.log(fileExtension);
+//     if (!files.includes(fileExtension)) {
+//       cleanedArray.push(file);
+//     }
+//   }
+
+//   console.log(
+//     "cleaned array with unwanted file extentions removed: ",
+//     cleanedArray
+//   );
+
+//   // iterate over files
+//   for (const file of cleanedArray) {
+//     // exclude package-lock file
+//     if (file === "package-lock.json") {
+//       continue;
+//     }
+
+//     const filePath = path.join(currentDirectory, file);
+
+//     // exclude node modules folder
+//     if (filePath.includes("node_modules/")) {
+//       continue;
+//     }
+
+//     const fileStat = await stat(filePath);
+//     if (fileStat.isDirectory()) {
+//       continue;
+//     }
+
+//     // read contents and append to final string array
+//     // TO-DO - add filenames and delimiters
+//     const content = await readFile(filePath, "utf-8");
+//     finalStringArray.push(content);
+//   }
+
+//   return finalStringArray;
+// }
+//(async function () {
+// await startingTokenCount();
+// const finalStringArray = await readFiles();
+// const finalString = finalStringArray.join("\n");
+// console.log("final string array: ", finalStringArray);
+// console.log("final string: ", finalString);
+//})();
 
 // recursively read files from current dir
 // count tokens
